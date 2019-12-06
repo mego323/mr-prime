724f0f5f2d320a220260e3d2f63d56f039a4602f
mego323 <51208498+mego323@users.noreply.github.com>
2019-12-06 10:30:02 +0200

    Add files via upload

---
+++ prime.sh
@@ -0,0 +1,529 @@
+#!/bin/bash
+#made by mr prime
+######
+
+back7(){
+ echo''
+ echo '(1)back to tool'
+ echo '(2)exit'
+ echo -n 'enter ->>';read a ;echo''
+ if [ $a -eq 1 ];then
+ bash prime.sh
+ fi
+ if [ $a -eq 2 ];then
+ exit
+ fi
+}
+
+######
+#first
+ddos1(){
+  
+back(){
+ echo''
+ echo '(1)back to tool'
+ echo '(2)exit'
+ echo -n 'enter ->>';read a ;echo''
+ if [ $a -eq 1 ];then
+ bash prime.sh
+ fi
+ if [ $a -eq 2 ];then
+ exit
+ fi
+}
+clear
+echo ''
+echo '|\/| |¯|   |¯| |¯| | |\/| |¯
+|  | |¯\   |¯  |¯\ | |  | |¯
+                           ¯ '
+ echo''
+ echo '(1)-hammer'
+ echo '(2)-slowloris'
+ echo '(3)-hulk'
+ echo '(4)-pentmenuddos'
+ echo '(5)-syper-attack'
+ echo '(6)-santent-online'
+ echo '(7)-blackhatislamicddos'
+ echo '(8)-golden eye'
+ echo '(9)-xerxes'
+ echo '(0)-back'
+ echo ''
+ echo -n 'enter ->>';read a ;echo''
+
+if [ $a -eq 1 ]; then
+clear
+figlet -f big 'hammer'
+git clone https://github.com/cyweb/hammer
+figlet -f big 'done'
+mv hammer $HOME 
+back
+fi
+if [ $a -eq 2 ]; then
+clear
+figlet -f big 'slowloris'
+gir clone https://github.com/gkbrk/slowloris
+figlet -f big 'done'
+mv slowloris $HOME
+back
+fi
+if [ $a -eq 3 ];then
+clear
+figlet -f big 'hulk'
+git clone https://github.com/grafov/hulk
+figlet -f big 'done'
+mv hulk $HOME
+back
+fi
+if [ $a -eq 4 ];then
+clear
+figlet -f big 'pentmenu'
+git clone https://github.com/GinjaChris/pentmenu
+figlet -f big 'done'
+mv pentmenu $HOME
+back
+fi
+if [ $a -eq 5 ];then
+clear
+figlet -f big 'syper-attack'
+git clone https://github.com/alialialiali111/syper-attack
+figlet -f big 'done'
+mv syper-attack $HOME
+back
+fi
+if [ $a -eq 6 ];then
+clear
+figlet -f big 'santent-online'
+git clone https://github.com/Gameye98/santet-online
+figlet -f big 'done'
+mv santet-online $HOME
+back
+fi
+if [ $a -eq 7 ] ;then
+clear
+figlet -f big 'bhi ddos'
+git clone https://github.com/DatabaseHk/blackhatislamicddos
+figlet -f big 'done'
+mv blackhatislamicddos $HOME
+back
+fi
+if [ $a -eq 8 ] ;then
+clear
+figlet -f big 'golden eye'
+git clone https://github.com/jseidl/GoldenEye
+figlet -f big 'done'
+mv GoldenEye $HOME
+back
+fi
+if [ $a -eq 9 ] ;then
+clear
+figlet -f big 'xerxes'
+git clone https://github.com/zanyarjamal/xerxes
+figlet -f big 'done'
+mv xerxes $HOME
+back
+fi
+if [ $a -eq 0 ];then
+bash prime.sh
+fi
+}
+######
+#second
+ccgen2(){
+ 
+back2(){
+ echo''
+ echo '(1)back to tool'
+ echo '(0)exit'
+ echo -n 'enter ->>';read cd ;echo''
+
+ if [ $cd -eq 1 ];then
+ bash prime.sh
+ fi
+ if [ $cd -eq 0 ];then
+ exit
+ fi
+ }
+######
+clear
+echo''
+echo '|\/| |¯|   |¯| |¯| | |\/| |¯ 
+|  | |¯\   |¯  |¯\ | |  | |¯ 
+                           ¯ '
+echo''
+echo '(1)-ccgen'
+echo '(2)-card number' 
+echo '(3)-zero eye'
+echo '(0)-back'
+echo ''
+echo -n 'enter ->>';read c ;echo''
+
+if [ $c -eq 1 ];then
+clear
+figlet -f big 'ccgen'
+git clone https://github.com/kuurtb/ccgen
+figlet -f big 'done'
+mv ccgen $HOME
+back2
+fi
+if [ $c -eq 2 ];then
+clear
+figlet -f big 'card number'
+git clone https://github.com/INDOnimous/Card-Number
+figlet -f big 'done'
+mv Card-Number $HOME
+back2
+fi
+if [ $c -eq 3 ];then
+clear
+figlet -f big 'zero eye'
+git clone https://github.com/m4rktn/zeroeye
+figlet -f big 'done'
+mv zeroeye $HOME
+back2
+fi
+if [ $c -eq 0 ];then
+bash prime.sh
+fi
+}
+#####
+fbhack3(){
+
+back3(){
+ echo '(1)back to tool'
+ echo '(2)exit'
+ echo -n 'enter ->>';read a ;echo''
+ if [ $a -eq 1 ];then
+ bash prime.sh
+ fi
+ if [ $a -eq 2 ];then
+ exit
+ fi
+}
+
+clear
+echo''
+echo '|\/| |¯|   |¯| |¯| | |\/| |¯ 
+|  | |¯\   |¯  |¯\ | |  | |¯ 
+                           ¯ '
+
+echo''
+echo '(1)-fb-group-hack'
+echo '(2)-dark-fb'
+echo '(3)-fb-cracker'
+echo '(4)-fb-brute'
+echo '(5)-hack-facebook-teman'
+echo '(6)-socail brute'
+echo '(0)-back'
+echo''
+echo -n 'enter ->>';read f ;echo''
+
+if [ $f	 -eq 1 ] ;then
+clear
+figlet -f big 'fb group'
+git clone https://github.com/Mr-NemrMedo/fb-group-hack
+figlet -f big 'done'
+mv fb-group-hack $HOME
+back3
+fi
+if [ $f -eq 2 ] ;then
+clear
+figlet -f big 'fb-gelap'
+git clone https://github.com/MiSetya/FB-Gelap
+figlet -f big 'done'
+mv FB-Gelap $HOME
+back3
+fi
+if [ $f -eq 3 ] ;then
+clear
+figlet -f big 'fb-cracker'
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+figlet -f big 'done'
+mv Fb-Cracker-v.3 $HOME
+back3
+fi
+if [ $f -eq 4 ] ;then
+clear
+figlet -f big 'fb-brute'
+git clone https://github.com/IqbalzNoobs/fb-brute
+figlet -f big 'done'
+mv fb-brute $HOME
+back3
+fi
+if [ $f -eq 5  ] ;then
+clear
+figlet -f big 'hack-facebook-teman'
+git clone https://github.com/blackcodercrush/hack-facebook-teman
+figlet -f big 'done'
+mv hack-facebook-teman $HOME
+back3
+fi
+if [ $f -eq 6  ] ;then
+clear
+figlet -f big 'social brute'
+git clone https://github.com/5h4d0wb0y/socialbrute.git
+figlet -f big 'done'
+mv socialbrute $HOME
+back3
+fi
+if [ $f -eq 0 ];then
+bash prime.sh
+fi
+}
+######
+insta4(){
+
+back4(){
+ echo''
+ echo '(1)-back to tool'
+ echo '(0)-exit'
+ echo''
+ echo -n 'enter ->>';read e ;echo''
+ if [ $e -eq 1 ];then
+ bash prime.sh
+ fi
+ if [ $e -eq 0 ];then
+ exit
+ fi
+}
+
+
+clear
+echo''
+echo '|\/| |¯|   |¯| |¯| | |\/| |¯
+|  | |¯\   |¯  |¯\ | |  | |¯
+                           ¯ '
+echo''
+echo '(1)-insta-brute'
+echo '(0)-back'
+echo''
+echo -n 'enter ->>';read i ;echo
+if [ $i -eq 1 ];then
+clear
+figlet -f big 'insta brute'
+git clone https://github.com/TERMUXID3/instabrute
+figlet -f big 'done'
+mv instabrute $HOME
+back4
+fi
+}
+######
+gmail5(){
+
+back5(){
+ echo''
+ echo '(1)-back to tool'
+ echo '(0)-exit'
+ echo''
+ echo 'enter ->>';read h ;echo''
+ if [ $h -eq 1 ];then
+ bash prime.sh
+ fi
+ if [ $h -eq 0 ];then
+ exit
+ fi
+}
+
+
+clear
+echo''
+echo '|\/| |¯|   |¯| |¯| | |\/| |¯
+|  | |¯\   |¯  |¯\ | |  | |¯
+                           ¯ '
+echo ''
+echo '(1)-black-hack'
+echo '(2)-gemail hack'
+echo '(3)-socail brute'
+echo '(4)-brute gmail'
+echo '(0)-back'
+echo''
+echo -n 'enter ->>';read g ;echo''
+
+if [ $g -eq 1 ] ;then
+clear
+figlet -f big 'black hack'
+git clone https://github.com/alialialiali111/Black-hack
+figlt -f big 'done'
+mv Black-hack $HOME
+back5
+fi
+if [ $g -eq 2 ] ;then
+clear
+figlet -f big 'gemail hack'
+git clone https://github.com/Ha3MrX/Gemail-Hack
+figlet -f big 'done'
+mv Gemail-Hack $HOME
+back5
+fi
+if [ $g -eq 3 ] ;then
+clear
+figlet -f big ''
+git clone https://github.com/5h4d0wb0y/socialbrute.git
+figlet -f big 'done'
+mv socialbrute $HOME
+back5
+fi
+if [ $g -eq 4 ] ;then
+clear
+figlet -f big 'brute gmail'
+git clone https://github.com/NeloF4/brutegmail
+figlet -f big 'done'
+mv brutegmail $HOME
+back5
+fi
+if [ $g -eq 0 ];then
+bash prime.sh
+fi
+}
+######
+spam6(){
+back6(){
+ echo '(1)back to tool'
+ echo '(2)exit'
+ echo -n 'enter ->>';read a ;echo''
+ if [ $a -eq 1 ];then
+ bash prime.sh
+ fi
+ if [ $a -eq 2 ];then
+ exit
+ fi
+}
+
+clear
+echo '(1)-spammer-grab'
+echo '(2)-c-lite spam'
+echo '(3)-spam sms'
+echo '(4)-fake call'
+echo '(5)-SPAMer'
+echo '(6)-spammer z'
+echo '(7)-prank'
+echo '(8)-spammer'
+echo '(9)-spam cs'
+echo '(0)-back'
+echo''
+echo -n 'enter ->>';read s ;echo''
+
+if [ $s -eq 1 ] ;then
+clear
+figlet -f big 'spammer grab'
+git clone https://github.com/Noxturnix/Spammer-Grab
+figlet -f big 'done'
+mv Spammer-Grab $HOME
+back6
+fi
+if [ $s -eq 2 ] ;then
+clear
+figlet -f big 'c-lite spam'
+git clone https://github.com/abhiyasaRamaS/C-LITESPAM
+figlet -f big 'done'
+mv C-LITESPAM $HOME
+back6
+fi
+if [ $s -eq 3 ] ;then
+clear
+figlet -f big 'spam sms'
+git clone https://github.com/KANG-NEWBIE/SpamSms
+figlet -f big 'done'
+mv SpamSms $HOME
+back6
+fi
+if [ $s -eq 4 ] ;then
+clear
+figlet -f big 'fake call'
+git clone https://github.com/siputra12/fakecall
+figlet -f big 'done'
+mv fakecall $HOME
+back6
+fi
+if [ $s -eq 5 ] ;then
+clear
+figlet -f big 'SPAMer'
+git clone https://github.com/Bl4ckDr460n/SPAMer
+figlet -f big 'done'
+mv SPAMer $HOME
+back6
+fi
+if [ $s -eq 6 ] ;then
+clear
+figlet -f big 'spammer z'
+git clone https://github.com/rezadkim/spammerZ
+figlet -f big 'done'
+mv spammerZ $HOME
+back6
+fi
+if [ $s -eq 7 ] ;then
+clear
+figlet -f big 'prank'
+git clone https://github.com/siputra12/prank
+figlet -f big 'done'
+mv prank $HOME
+back6
+fi
+if [$s -eq 8 ] ;then
+clear
+figlet -f big 'spammer'
+git clone https://github.com/MaulanaRyM/Spammer
+figlet -f big 'done'
+mv Spammer $HOME
+back6
+fi
+if [ $s -eq 9 ] ;then
+clear
+figlet -f big 'spam cs'
+git clone https://github.com/CrazyError/CS
+figlet -f big 'done'
+mv CS $HOME
+back6
+fi
+if [ $s -eq 0 ];then
+bash prime.sh
+fi
+}
+######
+######
+######
+clear
+echo ''
+echo '|\/| |¯|   |¯| |¯| | |\/| |¯
+|  | |¯\   |¯  |¯\ | |  | |¯
+                           ¯ '
+echo ''
+echo '(1)-ddos attack'
+echo '(2)-ccgen'
+echo '(3)-fb hack'
+echo '(4)-instagram hack'
+echo '(5)-gmail hack'
+echo '(6)-spam'
+echo '(7)-report'
+echo '(0)-exit'
+echo ''
+echo -n 'enter ->>';read d ;echo''
+
+if [ $d -eq 1 ];then
+ddos1
+fi
+if [ $d -eq 2 ];then
+ccgen2
+fi
+if [ $d -eq 3 ];then
+fbhack3
+fi
+if [ $d -eq 4 ];then
+insta4
+fi
+if [ $d -eq 5 ];then
+gmail5
+fi
+if [ $d -eq 6 ];then
+spam6
+fi
+if [ $d -eq 7 ];then
+clear
+figlet -f big 'report'
+git clone https://github.com/IlayTamvan/Report
+figlet -f big 'done'
+mv Report $HOME
+back7
+fi
+if [ $d -eq 0 ];then
+exit
+fi
